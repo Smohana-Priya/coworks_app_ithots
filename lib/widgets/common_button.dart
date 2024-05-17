3f8e734f512bd24bb7ff7c804ee71bb02299ee66
Smohana-Priya <smohanapriya2604@gmail.co>
2024-05-17 10:40:44 +0530

    folder setup

---
+++ lib/widgets/common_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CommonButton extends StatelessWidget {
+  const CommonButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/widgets/common_textfield.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CommonTextField extends StatelessWidget {
+  const CommonTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
