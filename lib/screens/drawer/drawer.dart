54ff5858d7f17ffce72caa126d311ce56f3db86e
Smohana-Priya <smohanapriya2604@gmail.co>
2024-05-25 19:39:18 +0530

    added navigation in drawer

--- lib/screens/drawer/drawer.dart
+++ lib/screens/drawer/drawer.dart
@@ -1,4 +1,8 @@
 import 'package:coworks_app_ithots/data/nav_items.dart';
+import 'package:coworks_app_ithots/screens/coworks/coworks.dart';
+import 'package:coworks_app_ithots/screens/directory/directory.dart';
+import 'package:coworks_app_ithots/screens/home.dart';
+import 'package:coworks_app_ithots/screens/services/services.dart';
 import 'package:coworks_app_ithots/utils/app_colors.dart';
 import 'package:coworks_app_ithots/utils/app_images.dart';
 import 'package:flutter/material.dart';
@@ -41,13 +45,35 @@ class CoworksDrawer extends StatelessWidget {
           for (int i = 0; i < navItems.length; i++)
             Padding(
               padding: const EdgeInsets.only(left: 30, bottom: 30),
-              child: Text(
-                navItems[i],
-                style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'Inter',
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600),
+              child: InkWell(
+                onTap: () {
+                  switch (i) {
+                    case 0:
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Home()));
+                      break;
+                    case 1:
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Coworks()));
+                      break;
+                    case 2:
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Directory()));
+                      break;
+                    case 3:
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Services()));
+                      break;
+                  }
+                },
+                child: Text(
+                  navItems[i],
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Inter',
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600),
+                ),
               ),
             ),
         ],
--- lib/screens/services/services.dart
+++ lib/screens/services/services.dart
@@ -5,6 +5,7 @@ import '../../utils/app_colors.dart';
 import '../../utils/app_const.dart';
 import '../../utils/app_images.dart';
 import '../drawer/drawer.dart';
+import '../home.dart';
 import '../profile/profile.dart';
 
 class Services extends StatefulWidget {
@@ -57,101 +58,110 @@ class _ServicesState extends State<Services> {
       'text': AppConst.services,
     },
   ];
+  void navigateToHome() {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => Home()));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        drawer: Drawer(
-          child: CoworksDrawer(),
-        ),
-        appBar: AppBar(
-          toolbarHeight: 65,
-          centerTitle: true,
-          title: Padding(
-            padding: const EdgeInsets.only(top: 18.0),
-            child: Image.asset(AppImages.logo),
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (didPop) {
+        navigateToHome();
+      },
+      child: SafeArea(
+        child: Scaffold(
+          drawer: Drawer(
+            child: CoworksDrawer(),
           ),
-          actions: [
-            Padding(
-              padding: const EdgeInsets.only(right: 20.0, top: 18),
-              child: InkWell(
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => Profile()));
-                  },
-                  child: Image.asset(AppImages.profile)),
-            )
-          ],
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  AppConst.services,
-                  style: TextStyle(
-                      fontFamily: 'Inter',
-                      fontSize: 26,
-                      fontWeight: FontWeight.w600,
-                      color: AppColors.black),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Wrap(
-                  spacing: 10,
-                  runSpacing: 10,
-                  children: [
-                    for (int i = 0; i < cardsData.length; i++)
-                      SizedBox(
-                        width: (MediaQuery.of(context).size.width / 2) - 25,
-                        child: InkWell(
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => ServiceDetails(
-                                          title: cardsData[i]['text']!,
-                                        )));
-                          },
-                          child: Card(
-                            elevation: 1,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15),
-                            ),
-                            color: Colors.white,
-                            child: Padding(
-                              padding: EdgeInsets.all(12.0),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Image.asset(cardsData[i]['image']!),
-                                  Align(
-                                    alignment: Alignment.centerRight,
-                                    child: Icon(Icons.navigate_next),
-                                  ),
-                                  SizedBox(height: 5),
-                                  Text(
-                                    cardsData[i]['text']!,
-                                    style: TextStyle(
-                                      fontFamily: "Inter",
-                                      fontSize: 13,
-                                      fontWeight: FontWeight.w500,
-                                      color: Colors.black,
+          appBar: AppBar(
+            toolbarHeight: 65,
+            centerTitle: true,
+            title: Padding(
+              padding: const EdgeInsets.only(top: 18.0),
+              child: Image.asset(AppImages.logo),
+            ),
+            actions: [
+              Padding(
+                padding: const EdgeInsets.only(right: 20.0, top: 18),
+                child: InkWell(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Profile()));
+                    },
+                    child: Image.asset(AppImages.profile)),
+              )
+            ],
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    AppConst.services,
+                    style: TextStyle(
+                        fontFamily: 'Inter',
+                        fontSize: 26,
+                        fontWeight: FontWeight.w600,
+                        color: AppColors.black),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Wrap(
+                    spacing: 10,
+                    runSpacing: 10,
+                    children: [
+                      for (int i = 0; i < cardsData.length; i++)
+                        SizedBox(
+                          width: (MediaQuery.of(context).size.width / 2) - 25,
+                          child: InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => ServiceDetails(
+                                            title: cardsData[i]['text']!,
+                                          )));
+                            },
+                            child: Card(
+                              elevation: 1,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                              color: Colors.white,
+                              child: Padding(
+                                padding: EdgeInsets.all(12.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Image.asset(cardsData[i]['image']!),
+                                    Align(
+                                      alignment: Alignment.centerRight,
+                                      child: Icon(Icons.navigate_next),
                                     ),
-                                  ),
-                                ],
+                                    SizedBox(height: 5),
+                                    Text(
+                                      cardsData[i]['text']!,
+                                      style: TextStyle(
+                                        fontFamily: "Inter",
+                                        fontSize: 13,
+                                        fontWeight: FontWeight.w500,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  ],
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      )
-                  ],
-                )
-              ],
+                        )
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
